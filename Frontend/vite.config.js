bf0f33737a79c03746240e5c64407a9eb06429f7
akshita-suresh <akshitasuresh2006@gmail.com>
2026-09-19 00:36:48 +0530

    add deployed backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://homelyhub-idj8.onrender.com',
         changeOrigin: true,
         secure: false,
       }
